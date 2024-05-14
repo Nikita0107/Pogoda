41e79acfb7ab8bb9c0c8d547d2badac70fee33b1
niksi <nik.sidoryak@mail.ru>
2024-05-14 17:33:20 +0300

    123

--- панда1.py
+++ панда1.py
@@ -10,7 +10,24 @@ fixed_df = pd.read_csv(r'C:\Users\niksi\PycharmProjects\pythonProject13\Date\bik
                        sep=',', encoding='latin1',
                        parse_dates=['Date'], dayfirst=True,
                        index_col='Date')
-# fixed_df['Berri 1'].plot()
-fixed_df.plot(figsize=(15, 10))
+# 3 строки DataFrame
+# print(fixed_df[:3])
+
+# первые 10 значений из столбца 'Berri 1'
+# print(fixed_df['Berri 1'][:10])
+
+# Строим график по данным из столбца 'Berri 1'
+fixed_df['Berri 1'].plot()
+
+# Добавляем подписи к осям и заголовок графика
+plt.xlabel('Дата')
+plt.ylabel('Количество')
+plt.title('График поездок на велосипеде по датам')
+# plt.show()
+
+# Строим график по всем данным DataFrame
+fixed_df.plot()
 plt.show()
 
+column_sum = fixed_df['Rachel1'].sum()
+print(column_sum)
